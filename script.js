c80a87f751e7abc2262495718d4578d8ec89441a
freshwindows <freshwindows262@gmail.com>
2024-07-13 19:14:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+    alert('Downloading FWA...');
+    // Здесь можно добавить логику для скачивания файла
+    // Например:
+     window.location.href = 'FreshWindowsActivatorV1.0.0.zip';
+});
